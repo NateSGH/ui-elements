39964cbeb96808206bff068ce0ba6f97b3b6f339
Nate <for.education.n@yandex.ru>
2021-08-12 13:50:05 +0200

    Image slider WIP: added structure, images, next and previous buttons logic

---
+++ Image slider/script.js
@@ -0,0 +1,52 @@
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+function removeActiveClasses(elements) {
+  elements.forEach((element) => {
+    element.classList.remove('active');
+  });
+}
+
+function addActiveClassToEl(element) {
+  element.classList.add('active');
+}
+
+function changeSliderImg(direction) {
+  let currentActiveId = 0;
+
+  const imgs = document.querySelectorAll('.slider-img');
+  imgs.forEach((img) => {
+    if (img.classList.contains('active') && currentActiveId === 0) {
+      currentActiveId = Number(img.dataset.id);
+    }
+  });
+
+  if (currentActiveId !== 0) {
+    removeActiveClasses(imgs);
+  }
+
+  switch (direction) {
+    case 'next':
+      if (currentActiveId >= imgs.length) {
+        currentActiveId = 0;
+      }
+      addActiveClassToEl(document.querySelector(`img[data-id="${currentActiveId + 1}"]`));
+      break;
+    case 'prev':
+      if (currentActiveId === 1) {
+        currentActiveId = 6;
+      }
+      addActiveClassToEl(document.querySelector(`img[data-id="${currentActiveId - 1}"]`));
+      break;
+    default:
+      break;
+  }
+}
+
+nextBtn.addEventListener('click', () => {
+  changeSliderImg('next');
+});
+
+prevBtn.addEventListener('click', () => {
+  changeSliderImg('prev');
+});
