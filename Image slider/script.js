03473fde40e27f130752d480770023c7b47f5373
Nate <for.education.n@yandex.ru>
2021-08-12 15:09:48 +0200

    Image slider WIP: added coloring of circles under img

--- Image slider/script.js
+++ Image slider/script.js
@@ -14,15 +14,18 @@ function addActiveClassToEl(element) {
 function changeSliderImg(direction) {
   let currentActiveId = 0;
 
+  const imgCircles = document.querySelectorAll('.slider-circle');
+
   const imgs = document.querySelectorAll('.slider-img');
   imgs.forEach((img) => {
-    if (img.classList.contains('active') && currentActiveId === 0) {
+    if (img.classList.contains('active')) {
       currentActiveId = Number(img.dataset.id);
     }
   });
 
   if (currentActiveId !== 0) {
     removeActiveClasses(imgs);
+    removeActiveClasses(imgCircles);
   }
 
   switch (direction) {
@@ -31,12 +34,18 @@ function changeSliderImg(direction) {
         currentActiveId = 0;
       }
       addActiveClassToEl(document.querySelector(`img[data-id="${currentActiveId + 1}"]`));
+      addActiveClassToEl(
+        document.querySelector(`.slider-circle[data-id="${currentActiveId + 1}"]`)
+      );
       break;
     case 'prev':
       if (currentActiveId === 1) {
         currentActiveId = 6;
       }
       addActiveClassToEl(document.querySelector(`img[data-id="${currentActiveId - 1}"]`));
+      addActiveClassToEl(
+        document.querySelector(`.slider-circle[data-id="${currentActiveId - 1}"]`)
+      );
       break;
     default:
       break;
